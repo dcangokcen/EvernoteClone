c0261cd16bfc025e142da49fe8ded434f4048260
dcangokcen <d.cangokcen@gmail.com>
2023-08-29 09:53:23 +0300

    Font Size and Font Family combo boxes are added

--- EvernoteClone_Demo/View/NotesWindow.xaml.cs
+++ EvernoteClone_Demo/View/NotesWindow.xaml.cs
@@ -25,6 +25,12 @@ namespace EvernoteClone_Demo.View
         public NotesWindow()
         {
             InitializeComponent();
+
+			var fontFamilies = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+			fontFamilyComboBox.ItemsSource = fontFamilies;
+
+			List<double> fontSizes = new List<double>() { 8,9,10,11,12,14,16,28,48};
+			fontSizeComboBox.ItemsSource = fontSizes;
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
@@ -65,6 +71,9 @@ namespace EvernoteClone_Demo.View
 
 			var selectedDecorations = contentRichTextBox.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
 			underlinebutton.IsChecked = (selectedDecorations != DependencyProperty.UnsetValue) && (selectedDecorations.Equals(TextDecorations.Underline));
+
+			fontFamilyComboBox.SelectedItem = contentRichTextBox.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+			fontSizeComboBox.Text = (contentRichTextBox.Selection.GetPropertyValue(Inline.FontSizeProperty)).ToString();
 		}
 
 		private void boldButton_Click(object sender, RoutedEventArgs e)
@@ -109,6 +118,19 @@ namespace EvernoteClone_Demo.View
 				(contentRichTextBox.Selection.GetPropertyValue(Inline.TextDecorationsProperty) as TextDecorationCollection).TryRemove(TextDecorations.Underline, out textDecorations);
 				contentRichTextBox.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, textDecorations);
 			}
+		}
+
+		private void fontFamilyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if(fontFamilyComboBox.SelectedItem != null)
+			{
+				contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, fontFamilyComboBox.SelectedItem);
+			}
+		}
+
+		private void fontSizeComboBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontSizeProperty, fontSizeComboBox.Text);
 		}
 	}
 }
